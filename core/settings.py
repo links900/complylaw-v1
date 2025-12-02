9b304499d9ec98a4eeffab79c2416637c8911a2c
hp202350 <hp_202350@outlook.com>
2025-12-02 21:13:52 +0500

    Fix: WhiteNoise middleware in correct position

--- core/settings.py
+++ core/settings.py
@@ -157,6 +157,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -167,7 +168,7 @@ MIDDLEWARE = [
     'django_htmx.middleware.HtmxMiddleware',
     'auditlog.middleware.AuditlogMiddleware',
     'django_ratelimit.middleware.RatelimitMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'core.urls'
